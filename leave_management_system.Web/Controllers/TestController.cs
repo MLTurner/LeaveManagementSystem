daa77627b7cf200e8eec18d0c1477395ba286ac6
Marissa Turner <marissa.turner@ncdoi.gov>
2024-09-24 16:35:05 -0400

    Add project files.

---
+++ leave_management_system.Web/Controllers/TestController.cs
@@ -0,0 +1,18 @@
+﻿using leave_management_system.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace leave_management_system.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            var data = new TestViewModel
+            {
+                Name = "Student of MVC Mastery",
+                DateofBirth = new DateTime(1954, 12, 01)
+            };
+            return View(data);
+        }
+    }
+}
---
+++ leave_management_system.Web/Models/TestViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace leave_management_system.Web.Models
+{
+    public class TestViewModel
+    {
+        public string Name { get; set; }
+        public DateTime? DateofBirth { get; set; }
+    }
+}
